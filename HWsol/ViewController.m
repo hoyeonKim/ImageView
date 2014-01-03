89c883f76361ad034c0d5f4da781fbdbe45503a3
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-03 14:01:36 +0900

    이미지뷰

--- HWsol/ViewController.m
+++ HWsol/ViewController.m
@@ -9,15 +9,46 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSInteger _currentimageindex;
+    
+}
+@property (weak, nonatomic) IBOutlet UIImageView *imgview;
 
 @end
 
 @implementation ViewController
+- (IBAction)nxtbtn:(id)sender {
+    _currentimageindex++;
+    if(_currentimageindex == 3){
+        [sender setEnabled:NO];
+    }
+    [self change];
+}
+- (IBAction)prevbtn:(id)sender {
+    _currentimageindex--;
+    
+    if(_currentimageindex == 0){
+        [sender setEnabled:NO];
+    }
+    
+        [self change];
+}
+
+-(void) change{
+    NSString *carName = [NSString stringWithFormat:@"c%d",(int) _currentimageindex];
+    NSString *imagePath =[[NSBundle mainBundle] pathForResource:carName ofType:@"jpg"];
+    
+    UIImage *currentImage = [UIImage imageWithContentsOfFile:imagePath];
+    self.imgview.image = currentImage;
+    
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _currentimageindex=1;
+    [self change];
 }
 
 - (void)didReceiveMemoryWarning
